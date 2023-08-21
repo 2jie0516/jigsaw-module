1e225c84baacbde84673925f5938fab653f1d406
2jie0516 <dhtpgks10044@gmail.com>
2023-08-21 22:23:27 +0900

    다른 모듈에서 base 의존성 주입 및 테스트 로그 설정

--- client/src/main/java/clientModule/StudentClient.java
+++ client/src/main/java/clientModule/StudentClient.java
@@ -1,11 +1,21 @@
 package clientModule;
 
+import baseModule.Chicken;
 import dbModule.DbService;
 import modelModule.Student;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import serviceModule.StudentService;
 
 public class StudentClient {
+    private static final Logger logger = LogManager.getLogger(StudentClient.class);
+    public StudentClient() {
+        logger.info("Student Client test log");
+    }
+
     public static void main(String[] args) {
+        Chicken chicken = new Chicken();
+
         StudentService service = new DbService();
         service.create(new Student());
         service.read("17SS0001");
--- db/src/main/java/dbModule/DbService.java
+++ db/src/main/java/dbModule/DbService.java
@@ -1,9 +1,16 @@
 package dbModule;
 
 import modelModule.Student;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import serviceModule.StudentService;
 
 public class DbService implements StudentService {
+    private static final Logger logger = LogManager.getLogger(DbService.class);
+
+    public DbService() {
+        logger.info("DB Service test log");
+    }
 
     @Override
     public String create(Student student) {
